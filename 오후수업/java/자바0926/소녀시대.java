8d02a94176be3997de0103c779e025f7aeac61cb
Sujeong <110653032+sjsjsjsue@users.noreply.github.com>
2022-09-26 17:28:11 +0900

    0926Java class

---
+++ 오후수업/java/자바0926/소녀시대.java
@@ -0,0 +1,38 @@
+package 자바0926;
+
+public class 소녀시대 {
+
+	public static String 멤버소개(String 멤버이름) {
+		return "제 이름은 "+멤버이름+" 입니다.";
+		
+		}
+		public static int 멤버나이조회( int 나이) {
+			return 나이;
+			/*
+			 * 접근지정자 : public static
+			 * 리턴타입 : int
+			 * 메소드이름 : 멤버나이조회
+			 * 파라미터타입 : int 나이
+			 * */
+		}
+	
+		public static void 멤버활동기간을조회하다() {
+			/*
+			 * 접근지정자 : public static
+			 * 리턴타입 : void
+			 * 메소드이름 : 멤버활동기간을조회하다()
+			 * 파라미터타입 : x
+			 * */
+		}
+		public static boolean 멤버남자친구여부를조회하다(boolean 남자친구있어) {
+			return 남자친구있어;
+			/*
+			 * 접근지정자 : public static
+			 * 리턴타입 : boolean
+			 * 메소드이름 : 멤버남자친구여부를조회하다
+			 * 파라미터타입 : boolean 남자친구있어
+			 * */
+		}
+		
+		
+}
