b259959a6bf861b5e64694f1d8c259983c326dcc
sjsjsjsue <esther106@naver.com>
2022-10-18 10:14:22 +0900

    table 만드는 법

---
+++ 오전수업/javascript/10월 JAVASCRIPT/1017-1021/1017-18(table 만드는법)/js(table만드는법).js
@@ -0,0 +1,19 @@
+var out="<table border = '1'>";
+for(var i=1; i<=data.col; i++){
+    out += "<tr>"
+    for(var j =1; j<=data.row; j++){
+        out += "<td widht ="+ data.width +"height ="+ data.height +"></td>"
+    }
+    out += "</tr>"
+}
+out += "</table>"
+draw.innertHTML = out;
+
+
+// table도 감싸져 있는 구조고, 
+// 이중 for문도 감싸져 있는 구조니까 같이 생각해봅시당!!
+// <table> // for문 밖에 table 써주고
+//     <tr> // 첫 번째 for문 안에 tr 올리고
+//         <td></td> // 두번째 for문 안에 td 써주기!!
+//     </tr>  
+// </table>
