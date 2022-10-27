79f313950e0b92c3a08079cbdac19ae8c1a36db8
sjsjsjsue <esther106@naver.com>
2022-10-27 13:50:43 +0900

    feat : 플렉스 아이템 배치, align, php

---
+++ 오전수업/javascript/10월 JAVASCRIPT/1024-1028/1027/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <?php require_once("head.html") ?>
+</head>
+<body>
+  <div id="wrap">
+    <?php require_once("header.html") ?>
+  </div>
+</body>
+</html>
