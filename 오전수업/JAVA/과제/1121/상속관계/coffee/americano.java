63b6ffb9b46e713a8329539d746ba556ba782110
sjsjsjsue <esther106@naver.com>
2022-11-18 18:10:41 +0900

    feat : 1121 과제 하다 말음

---
+++ 오전수업/JAVA/과제/1121/상속관계/coffee/americano.java
@@ -0,0 +1,9 @@
+package coffee;
+
+class americano extends coffee_main{
+  public americano(){}
+
+  public americano(int price, boolean hotIced boolean decaffeine){
+    super(price, hotIced, decaffeine);
+  }
+}
---
+++ 오전수업/JAVA/과제/1121/상속관계/coffee/coffee_main.java
@@ -0,0 +1,17 @@
+pakage coffee;
+
+class coffee_main{
+  
+  int price;
+  boolean hotIced;
+  boolean decaffeine;
+
+
+  public coffee_main() {}
+  
+  public coffee_main(int price, boolean hotIced, boolean decaffeine){
+    this.price = price;
+    this.hotIced = hotIced;
+    this.decaffeine =decaffeine;
+  } 
+}
