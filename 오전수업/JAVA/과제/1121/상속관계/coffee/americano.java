98441cda50afe6c9979154101e1d00f206e60621
Sujeong <110653032+sjsjsjsue@users.noreply.github.com>
2022-11-20 14:37:15 +0900

    Delete 오전수업/JAVA/과제/1121/상속관계 directory

--- 오전수업/JAVA/과제/1121/상속관계/coffee/americano.java
+++
@@ -1,9 +0,0 @@
-package coffee;
-
-class americano extends coffee_main{
-  public americano(){}
-
-  public americano(int price, boolean hotIced boolean decaffeine){
-    super(price, hotIced, decaffeine);
-  }
-}
