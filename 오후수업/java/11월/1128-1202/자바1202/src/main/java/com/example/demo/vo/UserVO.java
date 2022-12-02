b018f47a229a3bed00d81095bc952af25dfa3b27
Sujeong <110653032+sjsjsjsue@users.noreply.github.com>
2022-12-02 18:01:18 +0900

    feat : user

---
+++ 오후수업/java/11월/1128-1202/자바1202/src/main/java/com/example/demo/vo/UserVO.java
@@ -0,0 +1,12 @@
+package com.example.demo.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserVO {
+	private String id;
+	private String pw;
+	private String name;
+}
