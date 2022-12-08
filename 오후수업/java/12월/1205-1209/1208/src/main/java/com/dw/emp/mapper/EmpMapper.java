9d2bab5ec7c0baeee044004dc7cb176100416c1d
Sujeong <110653032+sjsjsjsue@users.noreply.github.com>
2022-12-08 18:00:46 +0900

    feat : study

---
+++ 오후수업/java/12월/1205-1209/1208/src/main/java/com/dw/emp/mapper/EmpMapper.java
@@ -0,0 +1,39 @@
+package com.dw.emp.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.dw.emp.vo.EmpVO;
+
+@Mapper
+public interface EmpMapper {
+
+	public List<EmpVO> selectEmp();
+	
+	public EmpVO selectEmpfindByEmpno(int empno);
+	
+	/**
+	 * @Since : 2022. 12. 8.
+	 * @Author : sue
+	 * @Return : int
+	 * @Comment : 사원 추가
+	 */
+	int insertEmp(EmpVO vo);
+	
+	/**
+	 * @Since : 2022. 12. 8.
+	 * @Author : sue
+	 * @Return : int
+	 * @Comment : 사원 수정
+	 */
+	int updateEmp(EmpVO vo);
+	
+	/**
+	 * @Since : 2022. 12. 8.
+	 * @Author : sue
+	 * @Return : int
+	 * @Comment : 사원 삭제
+	 */
+	int deleteEmp(int empno);
+}
